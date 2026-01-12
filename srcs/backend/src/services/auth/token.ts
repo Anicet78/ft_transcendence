4cc511c7c92c88448f63f482dfac5a8f5956ba74
LSV310 <anicet.gruet@gmail.com>
2026-01-12 18:46:26 +0100

    Documenting my functions

--- srcs/backend/src/services/auth/token.ts
+++ srcs/backend/src/services/auth/token.ts
@@ -9,10 +9,20 @@ fastify.register(fastifyJwt, {
 	sign: { expiresIn: "15m" }
 });
 
+/**
+ * Create and return the token for a user.
+ * @param {{userId: number, email: string}} payload User payload
+ * @returns {string} JWT token
+ */
 export function createAccessToken(payload: {userId: number, email: string}): string {
 	return fastify.jwt.sign(payload);
 }
 
+/**
+ * Check if the token is valid and hasn't expired.
+ * @param {string} token JWT token
+ * @returns {{userId: number, email: string}} User payload
+ */
 export function verifyAccessToken(token: string): {userId: number, email: string} {
 	return fastify.jwt.verify(token);
 }
