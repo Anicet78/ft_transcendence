5db31766a45ae2a214e50ee822ff37462ef2ed79
LSV310 <anicet.gruet@gmail.com>
2026-01-13 16:37:49 +0100

    Add type provider to fastify

--- srcs/backend/src/server.ts
+++ srcs/backend/src/server.ts
@@ -1,12 +1,16 @@
 import Fastify from 'fastify';
+import type { TypeBoxTypeProvider } from '@fastify/type-provider-typebox';
 
 const fastify = Fastify({
 	logger: true, // active le logging intégré
 });
 
+fastify.withTypeProvider<TypeBoxTypeProvider>();
+
 // Démarrage du serveur
 const start = async () => {
 	try {
+		// register routes here
 		await fastify.listen({ port: 3000 });
 		console.log('Server running on http://localhost:3000');
 	} catch (err) {
