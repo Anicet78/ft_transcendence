1b5e4a46223d662bc9385c27f5c77a5e2b332e70
ndabboussi <dabboussi.nina@gmail.com>
2026-01-14 12:41:56 +0100

    schema.prisma ok

--- srcs/backend/prisma_entrypoint.sh
+++ srcs/backend/prisma_entrypoint.sh
@@ -1,30 +1,13 @@
 #!/bin/sh
 
-# echo "Running db_entrypoint.sh..."
-# ls -l /run/secrets
+echo "Running prisma_entrypoint.sh..."
 DB_PASSWORD=$(cat /run/secrets/db_password)
-# echo "Password loaded: ${#DB_PASSWORD} chars"
 
 export DATABASE_URL="postgresql://postgres_user:${DB_PASSWORD}@db:5432/transcendence"
-
-echo $DATABASE_URL
+echo "DATABASE_URL=" $DATABASE_URL
 
 exec npx prisma db pull
 
-echo "DATABASE_URL loaded securely"
+echo "DATABASE_URL loaded securely: ${#DATABASE_URL} chars."
 
 exec "$@"
-
-
-# # Load DATABASE_URL from secret file
-# if [ -f /run/secrets/database_url ]; then
-#   export DATABASE_URL="$(cat /run/secrets/database_url)"
-# fi
-
-# echo "DATABASE_URL loaded: ${#DATABASE_URL} chars"
-
-# exec "$@"
-
-
-
-  # url = env("postgresql://postgres_user:hellopss@db:5432/transcendence")
