766640ec15fbcdbcd3ba53afa2522cd9a259d955
LSV310 <anicet.gruet@gmail.com>
2026-01-14 15:54:16 +0100

    Added register route

---
+++ srcs/backend/src/controllers/registerController.ts
@@ -0,0 +1,40 @@
+import { type FastifyReply, type FastifyRequest } from "fastify";
+import type { User } from "../models/userModel.js";
+import type { RegisterResponseType, RegisterType } from "../routes/registerRoute.js";
+import { hashPassword } from "../services/auth/password.js";
+import { createAccessToken } from "../services/auth/token.js";
+
+export async function postRegisterController(
+	request: FastifyRequest<{ Body: RegisterType }>,
+	reply: FastifyReply
+) {
+	const {firstname, lastname, username, email, password} = request.body;
+
+	//	if (user already exists in db):
+	//		return reply.code(409).send("already exist");
+
+	let hash: string | null = null;
+
+	try {
+		hash = await hashPassword(password);
+	} catch (err) {
+		request.log.error(err);
+		return reply.code(500).send("password hashing error");
+	}
+
+	const user: User = {
+		id: "1",
+		firstname: firstname,
+		lastname: lastname,
+		username: username,
+		email: email,
+		passwordHash: hash
+	};
+
+	//	if (!add user to db(user))
+	//		return reply.code(500).send("database issue");
+
+	const response: RegisterResponseType = {token: createAccessToken({userId: user.id, email: user.email}), user: user};
+
+	return reply.status(200).send(response);
+}
