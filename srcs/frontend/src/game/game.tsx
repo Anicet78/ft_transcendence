7d5b04202866a3fd5a4ee6b98eeb2219c514d8f8
LSV310 <anicet.gruet@gmail.com>
2026-02-13 20:42:15 +0100

    J'ai fais de la merde

--- srcs/frontend/src/game/game.tsx
+++ srcs/frontend/src/game/game.tsx
@@ -5,39 +5,124 @@ import { Box } from '@allxsmith/bestax-bulma';
 import { useAuth } from '../auth/AuthContext';
 import api from '../serverApi';
 import { useMutation } from '@tanstack/react-query';
-import { useEffect } from 'react';
+import { useEffect, useRef, useState } from 'react';
+import type { GameModule } from './build/game';
+import createModule from './build/game';
 
 const Game = () => {
 	const navigate = useNavigate();
 	const { user } = useAuth();
 	const { room, start, cancelStart } = useRoom()!;
 
+	const [gameSocket, setGameSocket] = useState<WebSocket | null>(null);
+	const [Module, setModule] = useState<GameModule | null>(null);
+	const canvasRef = useRef<HTMLCanvasElement | null>(null);
+
 	const mutation = useMutation({
 		mutationFn: () => api.post("/room/launch", room),
 	});
 
 	useEffect(() => {
-		if (user?.id === room?.hostId)
+		if (user?.id === room?.hostId) {
 			mutation.mutate();
+		}
 		else if (!start) {
 			navigate("/home");
 			return ;
 		}
 
-		return () => cancelStart();
+		const socket = new WebSocket('wss://localhost:8443/ws/');
+		setGameSocket(socket);
+
+		if (!socket) return ;
+
+		return () => {
+			if (start) return;
+
+			cancelStart();
+			socket.close();
+
+			if (!Module) return;
+
+			Module.finishGame();
+
+			if ((Module as any).ctx) {
+				const ext = (Module as any).ctx.getExtension('WEBGL_lose_context');
+				if (ext) ext.loseContext();
+			}
+		};
 	}, []);
 
-	if (mutation.isPending)
-		return <div>Verifying room</div>
+	useEffect(() => {
+		if (!canvasRef.current || mutation.isPending || !gameSocket || !user || !room) return;
+
+		const initWasm = async () => {
+			try {
+				const mod = await createModule({
+					canvas: canvasRef.current,
+					noInitialRun: true,
+					locateFile: (path: string, prefix: string) => {
+						if (path.endsWith(".data")) return `http://localhost:5173/game/build/${path}`;
+						return prefix + path;
+					},
+					onCppMessage: (obj: Object) => gameSocket.send(JSON.stringify(obj)),
+					sendResults: (obj: Object) => {
+						console.log(JSON.stringify(obj))
+						navigate("/home");
+					}
+				});
+
+				(window as any).onCppMessage = (mod as any).onCppMessage;
+				(window as any).sendResults = (mod as any).sendResults;
+
+				setModule(mod);
+				// Add username and session size
+				mod.callMain([user.id, 'username', room.roomId, room.players.length.toString(), "5"]);
+			} catch (e) {
+				console.error("Wasm Error:", e);
+			}
+		};
+
+		initWasm();
+	}, [mutation.isPending, gameSocket]);
+
+	useEffect(() => {
+		if (!gameSocket || !Module) return;
+
+		gameSocket.onmessage = async (event) => {
+			let data = event.data;
+			if (data instanceof Blob) data = await data.text();
+
+			try {
+				const json = JSON.parse(data);
+				if (Module.getMessage) Module.getMessage(json);
+			} catch (e) {
+				console.error("JSON parse error", e);
+			}
+		};
+
+		gameSocket.onclose = () => {
+
+		};
+
+		gameSocket.onerror = (err) => {
+			console.error(err);
+		};
+
+	}, [gameSocket, Module]);
+
+	if (mutation.isPending) {
+		return <div>Verifying room</div>;
+	}
 
 	if (mutation.isError) {
 		navigate("/home");
-		return ;
+		return;
 	}
 
 	return (
 		<Box  m="4" p="6" bgColor="grey-light" textColor="black" justifyContent='space-between'>
-			<canvas id="game-canvas" width="800" height="950" tabIndex={1}></canvas>
+			<canvas ref={canvasRef} id="game-canvas" width="800" height="950" tabIndex={1}></canvas>
 		</Box>
 	)
 }
---
+++ srcs/game/game-server/srcs/Session.cpp
@@ -0,0 +1,314 @@
+# include "Session.hpp"
+
+Session::Session(void): _maxNumPlayer(2), _running(0), _ended(0), _startTime(std::chrono::steady_clock::time_point{}),
+						_numPlayersFinished(0), _readyToRun(0), _timerBeforeRun(std::chrono::_V2::steady_clock::now()), _readyToRunStartTimer(0.0f)
+{
+	static std::string set = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+	int size = static_cast<int>(2 * sqrt(8 + 6 * (_maxNumPlayer - 1)));
+
+	for (int i = 0; i < 25; i++)
+	{
+		int nb = rand() % 62;
+		this->_sessionId.push_back(set[nb]);
+	}
+
+	_maps.emplace_back(1, 1, this->_sessionId);
+	_maps.back().setWaitingRoom();
+	_maps.emplace_back(size, size, this->_sessionId);
+	_maps.back().fillMap(_maxNumPlayer, 0);
+	_maps.emplace_back(size * 0.8, size * 0.8, this->_sessionId);
+	_maps.back().fillMap(_maxNumPlayer, 1);
+	printMap(_maps[1]);
+	printMap(_maps[2]);
+	this->linkMaps(_maps[1], _maps[2]);
+}
+
+Session::Session(int numPLayer):	_maxNumPlayer(numPLayer), _running(0), _ended(0), _startTime(std::chrono::steady_clock::time_point{}),
+									_numPlayersFinished(0), _readyToRun(0), _timerBeforeRun(std::chrono::_V2::steady_clock::now()), _readyToRunStartTimer(0.0f)
+{
+	static std::string set = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+	int size = static_cast<int>(2 * sqrt(8 + 6 * (_maxNumPlayer - 1)));
+
+	for (int i = 0; i < 25; i++)
+	{
+		int nb = rand() % 62;
+		this->_sessionId.push_back(set[nb]);
+	}
+
+	_maps.emplace_back(1, 1, this->_sessionId);
+	_maps.back().setWaitingRoom();
+	_maps.emplace_back(size, size, this->_sessionId);
+	_maps.back().fillMap(_maxNumPlayer, 0);
+	_maps.emplace_back(size * 0.8, size * 0.8, this->_sessionId);
+	_maps.back().fillMap(_maxNumPlayer, 1);
+	printMap(_maps[1]);
+	printMap(_maps[2]);
+	this->linkMaps(_maps[1], _maps[2]);
+}
+
+Session::~Session(void)
+{
+	std::cout << "session " << this->_sessionId << " has ended !!" << std::endl;
+}
+
+//-----------------------------------------------------------------
+
+void	Session::launch()
+{
+	quadList start, vide;
+	this->_running = true;
+	size_t pos = 0;
+	this->_startTime = std::chrono::_V2::steady_clock::now();
+	for (quadList &node : this->_maps[1].getNodes())
+	{
+		if (!node->getRoom() || node->getRoom()->getName() != "start")
+			continue ;
+		if (pos >= this->_players.size())
+			break ;
+		std::string roomId = this->_players[pos]->getRoom().getRoomId();
+		this->_players[pos]->setNode(node);
+
+		this->_players[pos]->setStartPos(pos);
+
+		this->_players[pos]->getWs()->unsubscribe(roomId);
+		this->_players[pos]->getWs()->subscribe(node->getRoom()->getRoomId());
+		// if (this->_players[pos]->getWs()->unsubscribe(roomId))
+		// 	std::cout << "unsubscribe from waiting room" << std::endl;
+		// if (this->_players[pos]->getWs()->subscribe(node->getRoom()->getRoomId()))
+		// 	std::cout << "subscribed to " << node->getRoom()->getRoomId() << std::endl;
+		pos++;
+	}
+}
+
+void	Session::sendToAll(Player &sender)
+{
+	for (auto &player : _players)
+	{
+		if (sender.getUid() == player->getUid())
+			continue ;
+		sendPlayerState(*player, *this, sender.getUid());
+	}
+}
+
+void	Session::linkMaps(Map &down, Map &up)
+{
+	down.link(up);
+}
+
+void	putRoomEvent(std::string &msg, std::shared_ptr<Room> room)
+{
+	msg += ", \"room_event\": \"" + room->getRoomEvent()->getType() + '\"';
+	if (room->getRoomEvent()->getType() == "MobRush")
+	{
+		MobRush &event = dynamic_cast<MobRush &>(*room->getRoomEvent());
+		std::unordered_map<int, std::unique_ptr<Mob>> &mobs = event.getMobs();
+		if (mobs.size())
+		{
+			msg += ", \"nbr_mob\": " + std::to_string(mobs.size());
+			msg += ", \"mobs\": [";
+			for (auto it = mobs.begin(); it != mobs.end(); ++it)
+			{
+				Mob	&mob = *it->second;
+				msg += "{\"mob_id\": " + std::to_string(it->first) + ", "
+					+ "\"mob_x\": " + std::to_string(mob.getX()) + ", "
+					+ "\"mob_y\": " + std::to_string(mob.getY()) + "},";
+			}
+			if (*msg.rbegin() == ',')
+				msg.pop_back();
+			msg += "]";
+		}
+	}
+}
+
+std::string	Session::sendMaps(void)
+{
+	if (!this->_mapInfos.empty())
+		return this->_mapInfos;
+
+	std::string msg = "{\"action\": \"waiting\", \"session_id\": \"" + this->_sessionId + "\", \"maps\": { ";
+	for (size_t i = 0; i < this->_maps.size(); i++)
+	{
+		if (!i)
+			msg += "\"waiting_map\": { ";
+		else
+			msg += ", \"floor_" + std::to_string(i - 1) + "\": { ";
+		msg += "\"size_x\": " + std::to_string(this->_maps[i].getWidth()) + ", "
+			+ "\"size_y\": " + std::to_string(this->_maps[i].getHeight()) + ", "
+			+ "\"rooms\": [";
+		int j = 0;
+		for (quadList &node : this->_maps[i].getNodes())
+		{
+			if (!node->getRoom())
+				continue ;
+			auto room = node->getRoom();
+			if (j)
+				msg += ", ";
+			msg += "{\"name\": \"" + room->getName() + "\", "
+				+ "\"x\": " + std::to_string(node->getX()) + ", "
+				+ "\"y\": " + std::to_string(node->getY()) + ", "
+				+ "\"rot\": " + std::to_string(room->getRotated());
+			if (room->getRoomEvent())
+			{
+				putRoomEvent(msg, room);
+			}
+			msg += '}';
+			j++;
+		}
+		msg += "], \"nb_rooms\": " + std::to_string(j) + "}";
+	}
+	msg += "}}";
+	this->_mapInfos = msg;
+	return this->_mapInfos;
+}
+
+void	Session::addParty(Party &newParty)
+{
+	std::string msg;
+	for (std::shared_ptr<Player> &player : newParty.getPlayers())
+	{
+		player->setNode(this->_maps[0].getNodes()[0]);
+		if (player->getWs()->subscribe(player->getRoom().getRoomId()))
+			std::cout << "added to the waiting room" << std::endl;
+		this->_players.push_back(player);
+		msg = this->sendMaps();
+		player->getWs()->send(msg);
+        player->getWs()->send("You have been added to a session !", uWS::OpCode::TEXT);
+	}
+}
+
+bool	Session::removePlayer(std::shared_ptr<Player> rmPlayer)
+{
+	for (size_t i = 0; i < this->_players.size(); i++)
+	{
+		if (this->_players[i]->getUid() == rmPlayer->getUid())
+		{
+			this->_players.erase(this->_players.begin() + i);
+			return 1;
+		}
+	}
+	return 0;
+}
+
+std::vector<std::shared_ptr<Player>>	Session::getPlayers() const
+{
+	return this->_players;
+}
+
+std::shared_ptr<Player> &Session::getPlayer(std::string &uid)
+{
+	for (auto &player : _players)
+	{
+		if (player->getUid() == uid)
+		{
+			return player;
+			break ;
+		}
+	}
+	return _players[0];
+}
+
+double	Session::getActualTime(void) const
+{
+	if (_startTime == std::chrono::steady_clock::time_point{})
+	    return 0;
+	return std::chrono::duration<double>(std::chrono::steady_clock::now() - this->_startTime).count();
+}
+
+int	Session::getMaxNumPlayer() const
+{
+	return this->_maxNumPlayer;
+}
+
+int	Session::getPlaceLeft() const
+{
+	return this->_maxNumPlayer - this->_players.size();
+}
+
+int	Session::getNumPlayers() const
+{
+	return this->_players.size();
+}
+
+bool	Session::hasEnded() const
+{
+	return this->_ended;
+}
+
+bool	Session::isRunning() const
+{
+	return this->_running;
+}
+
+bool Session::isReadyToRun() const
+{
+	return this->_readyToRun;
+}
+
+bool Session::doesAllPlayersConnected() const
+{
+	for (auto &player : this->_players)
+		if (!player->isConnected())
+			return false;
+	return true;
+}
+
+bool	Session::isPlayerInSession(std::string &uid) const
+{
+	for (auto &player : _players)
+		if (player->getUid() == uid)
+			return true;
+	return false;
+}
+
+void	Session::checkFinishedPlayers(uWS::App &app)
+{
+	std::vector<std::shared_ptr<Player>> finishedPlayers;
+	for (auto &player : this->_players)
+	{
+		if (player->getFinished())
+		{
+			this->_numPlayersFinished++;
+			int win = (this->_numPlayersFinished == 1) ? true : false;
+			player->setHasWin(win);
+			std::string msg = "{ \"action\": \"finished\", \"time\": "
+				+ std::to_string(this->getActualTime()) + ", \"win\": "
+				+ std::to_string(win) + "}";
+			player->getWs()->send(msg);
+			std::string	oldTopic = player->getRoomRef().getRoomId();
+			sendLeaveUpdate(*player, app, oldTopic);
+			player->getWs()->unsubscribe(oldTopic);
+			// if (player->getWs()->unsubscribe(oldTopic))
+			// 	std::cout << "unsibscribe from " << oldTopic << std::endl;
+			finishedPlayers.push_back(player);
+			std::cout << player->getName() << ": " << std::endl
+				<< "Kills: " << player->getKills() << "Place :" << player->getFinalRanking() << std::endl;
+		}
+	}
+	
+	if (!this->_players.size() && this->_running)
+	{
+		std::cout << "SESSION STOP" << std::endl;
+		this->_ended = 1;
+	}
+}
+
+double Session::getActualTimeBeforeRun(void) const
+{
+	return std::chrono::duration<double>(std::chrono::steady_clock::now() - this->_timerBeforeRun).count();
+}
+
+void	Session::startLaunching(void)
+{
+	if (this->_running || this->_readyToRun)
+		return ;
+	this->_readyToRun = true;
+	this->_readyToRunStartTimer = getActualTimeBeforeRun();
+	return ;
+}
+
+bool	Session::isEnoughtReadyTime(void) const
+{
+	if (this->getActualTimeBeforeRun() - this->_readyToRunStartTimer > 1)
+		return(true);
+	return(false);
+}
